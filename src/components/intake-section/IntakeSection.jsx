45ed17266996d34057b4a330bcbbec2d5c97e630
mdarif6 <md1arif015@gmail.com>
2023-04-01 01:18:39 +0530

    adding question components

--- src/components/intake-section/IntakeSection.jsx
+++ src/components/intake-section/IntakeSection.jsx
@@ -1,11 +1,56 @@
+import Question from "../question/Question";
 import "./IntakeSection.css";
 export default function IntakeSection() {
   return (
-    <div>
+    <div className="expent-intakesection">
       <div className="expent-intake-infobtns">
         <button>General Information</button>
         <button>Security & Privacy</button>
         <button>Upload Documents</button>
+      </div>
+
+      <div className="expent-info-components">
+        <div className="expent-general-information">
+          <p className="expent-info-heading">General Information</p>
+          <div className="expent-question-components">
+            <Question
+              questionNumber={"1"}
+              questionText={"Please select your sourcing option"}
+              optionText={"Choose Sourcing"}
+            />
+            <Question
+              questionNumber={"2"}
+              questionText={"Number range of users who will use the product:"}
+              optionText={"Choose Sourcing"}
+            />
+            <Question
+              questionNumber={"3"}
+              questionText={"Provided an expected spend:"}
+              optionText={"Choose expense range"}
+            />
+          </div>
+        </div>
+
+        <div className="expent-general-information">
+          <p className="expent-info-heading">Product Feedback</p>
+          <div className="expent-question-components">
+            <Question
+              questionNumber={"1"}
+              questionText={"Please select your sourcing option"}
+              optionText={"Choose Sourcing"}
+            />
+            <Question
+              questionNumber={"2"}
+              questionText={"Number range of users who will use the product:"}
+              optionText={"Choose Sourcing"}
+            />
+            <Question
+              questionNumber={"3"}
+              questionText={"Provided an expected spend:"}
+              optionText={"Choose expense range"}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
